2a65c2e882ed81b8b64a982dd41851f484ce0a3b
musausman <81835482+musausman@users.noreply.github.com>
2021-04-04 15:08:51 +0100

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,34 @@
+name = "MUSA USMAN"
+department = "COMPUTER SCINCE"
+print(name)
+print (department)
+
+scare = "boo"
+print(scare)
+
+variable_1= "python"
+variable_2= "coding"
+variable_3 = "variable_2+variable_1"
+print(variable_1)
+print(variable_2)
+print(variable_3)
+
+
+
+division = 12 / .5
+print(division)
+
+
+num1 = 10
+num2 = 15
+print(num1+num2)
+
+
+a = 8
+b = 3
+print(8 % 3)
+
+
+a = 2 * 4
+b = 4 + 2
+print(a*b)
